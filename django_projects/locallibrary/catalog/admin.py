4dd1245410d5e73856b2c973fb341438c82c6bd5
polynova <jocelyncasanova1@gmail.com>
2019-06-20 18:00:18 -0400

    added genere display to books in catalog

--- django_projects/locallibrary/catalog/admin.py
+++ django_projects/locallibrary/catalog/admin.py
@@ -23,7 +23,7 @@ admin.site.register(Genre)
 # Register the Admin classes for Book using the decorator
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'author', 'display_genre')
 
 
 # Register the Admin classes for BookInstance using the decorator
